9065ab916bee0f41ef1f43f16c587b9f3b62a72e
IndecisionTree <84244762+IndecisionTree@users.noreply.github.com>
2022-12-01 07:03:54 -0500

    feat(day1): solve day 1

--- solutions/Days/Day01.hs
+++ solutions/Days/Day01.hs
@@ -2,15 +2,19 @@ module Days.Day01 (day01) where
 
 import AOC (Solution (..))
 import qualified Data.Text as T
+import qualified Data.Text.Read as T
+import Data.List (sortBy)
 
 day01 :: Solution
 day01 = Solution parseInput part1 part2
 
-parseInput :: T.Text -> a
-parseInput = error "parseInput not defined for day 01"
+parseInput :: T.Text -> [Int]
+parseInput = fmap (sum . fmap readInt . T.lines) . T.splitOn "\n\n"
+  where
+    readInt = either error fst . T.decimal
 
-part1 :: a -> Int
-part1 = error "part1 not defined for day 01"
+part1 :: [Int] -> Int
+part1 = maximum
 
-part2 :: a -> Int
-part2 = error "part2 not defined for day 01"
+part2 :: [Int] -> Int
+part2 = sum . take 3 . sortBy (flip compare)
