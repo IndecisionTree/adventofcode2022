2278f70ac9e468e59e6f7aedc2b519a15d7cd6b7
IndecisionTree <84244762+IndecisionTree@users.noreply.github.com>
2022-12-02 08:50:19 -0500

    refactor(day2): derive an Enum instance

--- solutions/Days/Day02.hs
+++ solutions/Days/Day02.hs
@@ -7,23 +7,11 @@ import qualified Data.Text as T
 
 day02 :: Solution
 day02 = Solution parseInput part1 part2
-
 data RPS = Rock | Paper | Scissors
-  deriving (Show, Eq, Bounded)
+  deriving (Show, Eq, Enum, Bounded)
 
 data Action = Lose | Draw | Win
   deriving (Show, Eq)
-
-instance Enum RPS where
-  fromEnum rps = case rps of
-    Rock -> 1
-    Paper -> 2
-    Scissors -> 3
-  toEnum n = case n of
-    1 -> Rock
-    2 -> Paper
-    3 -> Scissors
-    _ -> error $ "Cannot convert to RPS: " ++ show n
 
 parseInput :: T.Text -> [(RPS, T.Text)]
 parseInput = fmap pLine . T.lines
@@ -40,19 +28,21 @@ part2 :: [(RPS, T.Text)] -> Int
 part2 = sum . fmap (score . matchAction . second (translate table))
   where
     table = [("X", Lose), ("Y", Draw), ("Z", Win)]
-    matchAction (rps, action) = (rps, match)
-      where
-        match = case action of
-          Lose -> prevRPS rps
-          Draw -> rps
-          Win -> nextRPS rps
 
 translate :: (Show a, Eq a) => [(a, b)] -> a -> b
 translate table ch =
   fromMaybe (error $ "Lookup failed: " ++ show ch) $ lookup ch table
 
+matchAction :: (RPS, Action) -> (RPS, RPS)
+matchAction (rps, action) = (rps, match)
+  where
+    match = case action of
+      Lose -> prevRPS rps
+      Draw -> rps
+      Win -> nextRPS rps
+
 score :: (RPS, RPS) -> Int
-score (a, b) = fromEnum b + beats a b
+score (a, b) = (fromEnum b + 1) + beats a b
 
 beats :: RPS -> RPS -> Int
 beats a b
@@ -60,10 +50,10 @@ beats a b
   | a == b = 3
   | otherwise = 0
 
-nextRPS :: RPS -> RPS
-nextRPS rps | rps == maxBound = minBound
-            | otherwise = succ rps
-  
-prevRPS :: RPS -> RPS
-prevRPS rps | rps == minBound = maxBound
-            | otherwise = pred rps
+nextRPS, prevRPS :: RPS -> RPS
+nextRPS rps
+  | rps == maxBound = minBound
+  | otherwise = succ rps
+prevRPS rps
+  | rps == minBound = maxBound
+  | otherwise = pred rps
