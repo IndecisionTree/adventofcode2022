a0ff2bd5b36dad28259a31430a4221f9bcf46d3d
IndecisionTree <84244762+IndecisionTree@users.noreply.github.com>
2022-12-03 01:06:20 -0500

    feat(day3): solve day 3

--- solutions/Days/Day03.hs
+++ solutions/Days/Day03.hs
@@ -1,16 +1,49 @@
 module Days.Day03 (day03) where
 
 import AOC (Solution (..))
-import qualified Data.Text as T
+import Control.Monad (join)
+import Data.Bifunctor (bimap)
+import Data.Char (isAsciiUpper, isAsciiLower)
+import Data.IntMap.Strict qualified as M
+import Data.Text qualified as T
+
+type Counter = M.IntMap Int
 
 day03 :: Solution
 day03 = Solution parseInput part1 part2
 
-parseInput :: T.Text -> a
-parseInput = error "parseInput not defined for day 03"
+parseInput :: T.Text -> [T.Text]
+parseInput = T.lines
 
-part1 :: a -> Int
-part1 = error "part1 not defined for day 03"
+part1 :: [T.Text] -> Int
+part1 = sum
+  . fmap (fst
+          . head
+          . M.toList
+          . uncurry M.intersection
+          . join bimap mkCounter
+          . (T.splitAt =<< ((`div` 2) . T.length)))
 
-part2 :: a -> Int
-part2 = error "part2 not defined for day 03"
+part2 :: [T.Text] -> Int
+part2 = sum
+  . fmap (fst . head . M.toList . foldr1 M.intersection)
+  . chunksOf 3
+  . fmap mkCounter
+
+priority :: Char -> Int
+priority ch
+  | isAsciiLower ch = fromEnum ch - 96
+  | isAsciiUpper ch = fromEnum ch - 38
+  | otherwise = error $ show ch
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf = go []
+  where
+    go a _ [] = a
+    go a n xs
+      | length xs >= n = go (take n xs : a) n (drop n xs)
+      | otherwise = reverse $ xs : a
+
+mkCounter :: T.Text -> Counter
+mkCounter =
+  M.fromListWith (+) . flip zip [1,1..] . fmap priority . T.unpack
