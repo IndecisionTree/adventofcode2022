e42adf8c83f8cc4ec52a9feddb1796e47cdf32c0
IndecisionTree <84244762+IndecisionTree@users.noreply.github.com>
2022-12-06 00:24:14 -0500

    feat(day6): solve day 6

--- solutions/Days/Day06.hs
+++ solutions/Days/Day06.hs
@@ -2,15 +2,23 @@ module Days.Day06 (day06) where
 
 import AOC (Solution (..))
 import qualified Data.Text as T
+import Data.List (find, nub)
+import Data.Maybe (fromJust)
 
 day06 :: Solution
 day06 = Solution parseInput part1 part2
 
-parseInput :: T.Text -> a
-parseInput = error "parseInput not defined for day 06"
+parseInput :: T.Text -> String
+parseInput = T.unpack
 
-part1 :: a -> Int
-part1 = error "part1 not defined for day 06"
+part1 :: String -> Int
+part1 = findUniq 4
 
-part2 :: a -> Int
-part2 = error "part2 not defined for day 06"
+part2 :: String -> Int
+part2 = findUniq 14
+
+findUniq :: Eq a => Int -> [a] -> Int
+findUniq n xs = (+n) $ fromJust $ find f [0..length xs - n]
+  where
+    f i = s == nub s
+      where s = take n $ drop i xs
