f5f49c5bfae04422a2fde1e7ef687a64a68d1360
IndecisionTree <84244762+IndecisionTree@users.noreply.github.com>
2022-12-05 20:02:59 -0500

    feat(day5): solve day 5

--- solutions/Days/Day05.hs
+++ solutions/Days/Day05.hs
@@ -1,16 +1,59 @@
 module Days.Day05 (day05) where
 
 import AOC (Solution (..))
-import qualified Data.Text as T
+import Control.Arrow
+import Data.List (foldl', transpose)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Text qualified as T
 
 day05 :: Solution
 day05 = Solution parseInput part1 part2
 
-parseInput :: T.Text -> a
-parseInput = error "parseInput not defined for day 05"
+-- TODO clean up this entire day, it's gross 🤮. Regex would probably
+-- work nice here as the input is regular.
 
-part1 :: a -> Int
-part1 = error "part1 not defined for day 05"
+type Instruction = (Int, Int, Int)
 
-part2 :: a -> Int
-part2 = error "part2 not defined for day 05"
+-- TODO this is a place where Text is annoying and 'Solution' is
+-- cumbersome to work with.  just end up unpacking it. Consider using
+-- Stringlike or something else.
+
+-- regex would be great here
+parseInput :: T.Text -> ([String], [Instruction])
+parseInput = T.unpack >>> splitOn "\n\n" >>> head &&& last >>> pCrates *** pMoves
+  where
+    pCrates = fmap (concat . init) . transpose . fmap pLine . lines
+    pLine = fmap pCrate . chunksOf 4
+    pCrate xs = [ch | ch /= ' ']
+      where ch = xs !! 1
+
+    pMoves = fmap pMove . lines
+    pMove xs = (read n, read from - 1, read to - 1)
+      where [_, n, _, from, _, to] = splitOn " " xs
+
+part1 :: ([String], [Instruction]) -> String
+part1 = fmap head . move reverse 
+
+part2 :: ([String], [Instruction]) -> String
+part2 = fmap head . move id
+
+-- | Given a move function and some stacks and instructions, perform
+-- the instructions on stacks using the move function.
+move :: (String -> String) -> ([String], [Instruction]) -> [String]
+move moveF (stacks, ins) = foldl' f stacks ins
+  where
+    f xs (n, from, to) = xs''
+      where
+        fromS = xs !! from
+        toS = xs !! to
+        xs' = update from (drop n fromS) xs
+        xs'' = update to (moveF (take n fromS) ++ toS) xs'
+
+-- TODO consider using Array or Vector (potentially mutable?) instead
+-- of this update nonsense
+
+-- | Replace an element at position 'n' in a list
+update :: Int -> a -> [a] -> [a]
+update n x xs
+  | n < length xs = uncurry (++) . second ((x:) . tail) $ splitAt n xs
+  | otherwise = error "Index out of bounds"
